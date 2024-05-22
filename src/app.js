0dd41d28ae6e37e44be60d7ca8d9298bbd5ecc11
kuntal Maity <143471213+kuntal45@users.noreply.github.com>
2024-05-22 09:19:11 +0000

    chore: Add error handling middleware for handling server errors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import errorHandler from "./middlewares/errorHendeler.meddleware";
 
 const app = express();
 
@@ -15,5 +16,5 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 
-
+app.use(errorHandler);
 export default app;
