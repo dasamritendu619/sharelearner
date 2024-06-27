6a97f4b34d8f40b32733bdee54cbc60f731bf96e
Sayan Maity <156517005+Sayancemk@users.noreply.github.com>
2024-06-27 06:37:45 +0000

    changeEmail case added

--- src/utils/resend.js
+++ src/utils/resend.js
@@ -540,6 +540,254 @@ If you are not request to reset your paasword, please ignore this mail.
 </html>
 
      `
+     break;
+    case "changeEmail":
+        subject=" change  your email for your Share Learner account";
+        html=`
+          <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style type="text/css">
+    #outlook a {
+      padding: 0;
+    }
+    .ReadMsgBody {
+      width: 100%;
+    }
+
+    .ExternalClass {
+      width: 100%;
+    }
+
+    .ExternalClass * {
+      line-height: 100%;
+    }
+
+    body {
+         </tr>
+              <tr>
+                <td class="main-content" style="padding: 48px 30px 40px; color: #000000;" bgcolor="#ffffff">
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 0 0 24px 0; font-size: 18px; line-height: 150%; font-weight: bold; color: #000000; letter-spacing: 0.01em;">
+                          Welcome, ${fullName}!
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          Thanks for choosing <b> share learner.</b> We are happy to see you on Dashboard.
+                          Ready to level up your communication skills and increase your connection
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          To get started, verify by this OTP:
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 24px 0;">
+                          <p class="button" title="Login OTP" style="width: 100%; background: #4C83EE; text-decoration: none; display: inline-block; padding: 10px 0; color: #fff; font-size: 14px; line-height: 21px; text-align: center; font-weight: bold; border-radius: 7px;">${OTP}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          If you need some help and feel any problem contact us on our support team:<a href="mailto: sharelearner2024@gmail.com">support@gmail.com</a>
+
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 8px;">
+                          <a style="display: flex; justify-content: space-between; align-items: center; padding: 28px 24px; border-radius: 4px; background-color: #FFF9F9; text-decoration: none;" href="#">
+                            <span style="width: 90%; font-size: 14px; line-height: 150%; font-weight: bold; color: #29426E; letter-spacing: 0.01em;">Get our new feature access</span>
+                            <span style="width: 10%; float: right;">
+                              <strong>→</strong>
+                            </span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 60px;">
+                          <a style="display: flex; padding: 28px 24px; border-radius: 4px; background-color: #FFF9F9; text-decoration: none;" href="#">
+                            <span style="width: 90%; font-size: 14px; line-height: 150%; font-weight: bold; color: #29426E; letter-spacing: 0.01em;">Find your connection and post</span>
+                            <span style="width: 10%; float: right;">
+                              <strong>→</strong>
+                            </span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                      <tr>
+                        <td style="padding: 0 0 16px;">
+                          <span style="display: block; width: 117px; border-bottom: 1px solid #8B949F;padding-left:0px"></span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="font-size: 14px; line-height: 170%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          Best regards, <br>
+                          <img style='width:90px'src='https://res.cloudinary.com/dqufodszt/image/upload/v1716363215/sharelerner/Main_1_u89enm.png
+'>   
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 24px 0 48px; font-size: 0px;">
+                       <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:300px;">      <![endif]-->
+                  <div class="outlook-group-fix" style="padding: 0 0 20px 0; vertical-align: top; display: inline-block; text-align: center; width:100%;">
+                    <span style="padding: 0; font-size: 11px; line-height: 15px; font-weight: normal; color:purple;">SHARE LERANER<br/>kolaghat,Purba Medinipur,West Bengal,India</span>
+                  </div>
+                     </td></tr></table>      <![endif]-->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>      margin: 0;
+      padding: 0;
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+
+    table,
+    td {
+      border-collapse: collapse;
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+
+  </style>
+ 
+  <style type="text/css">
+    @media only screen and (max-width:480px) {
+      @-ms-viewport {
+        width: 320px;
+      }
+      @viewport {
+        width: 320px;
+      }
+    }
+  </style>
+ 
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+  </style>
+ 
+  <style type="text/css">
+    @media only screen and (max-width:595px) {
+      .container {
+        width: 100% !important;
+      }
+      .button {
+        display: block !important;
+        width: auto !important;
+      }
+    }
+  </style>
+</head>
+
+<body style="font-family: 'Inter', sans-serif; background: #E5E5E5;">
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+    <tbody>
+      <tr>
+        <td valign="top" align="center">
+          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+            <tbody>
+<img  style='width:90px'src='https://res.cloudinary.com/dqufodszt/image/upload/v1716363215/sharelerner/Main_1_u89enm.png'</img>
+              <tr>
+                <td style="padding:8px 0 30px 0; text-align: center; font-size: 14px; color: #4C83EE;">
+                  SHARE LEARNER
+                </td>
+            <tr>
+                      </tr>
+              <tr>
+                <td class="main-content" style="padding: 48px 30px 40px; color: #000000;" bgcolor="#ffffff">
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 0 0 24px 0; font-size: 18px; line-height: 150%; font-weight: bold; color: #000000; letter-spacing: 0.01em;">
+                          Want to change your Email?
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          Thanks for choosing <b> share learner.</b>
+                          <br>
+                          <br> We received a request to reset your Email account for your share learner account. 
+                        </td>
+                      </tr>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 24px 0;">
+                          <p class="button" title="Reset Password" style="width: 100%; background: #4C83EE; text-decoration: none; display: inline-block; padding: 10px 0; color: #fff; font-size: 14px; line-height: 21px; text-align: center; font-weight: bold; border-radius: 7px;">${OTP}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px 0; font-size: 14px; line-height: 150%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+
+If you are not request to reset your email, please ignore this mail.
+<br>
+<br>
+                          If you need some help and feel any problem contact us on our support team:<a href="mailto: sharelearner2024@gmail.com">support@gmail.com</a>
+
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 8px;">
+                          
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 60px;">
+                         
+                            
+ </td>
+                      </tr>
+                      <tr>
+                      <tr>
+                        <td style="padding: 0 0 16px;">
+                          <span style="display: block; width: 117px; border-bottom: 1px solid #8B949F;padding-left:0px"></span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="font-size: 14px; line-height: 170%; font-weight: 400; color: #000000; letter-spacing: 0.01em;">
+                          Best regards, <br>
+                          <img style='width:90px'src=https://res.cloudinary.com/dqufodszt/image/upload/v1716363215/sharelerner/Main_1_u89enm.png
+   
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 24px 0 48px; font-size: 0px;">
+                  <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:300px;">      <![endif]--><div class="outlook-group-fix" style="padding: 0 0 20px 0; vertical-align: top; display: inline-block; text-align: center; width:100%;">
+                    <span style="padding: 0; font-size: 11px; line-height: 15px; font-weight: normal; color:purple;">SHARE LEARNER<br/>Kolaghat,Purba Medinipur,West Bengal,India</span>
+                  </div>
+                  <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>      
+             `
       break;
     default:
       subject = "Hello Learner";
